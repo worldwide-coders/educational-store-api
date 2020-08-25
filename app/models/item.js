d6ea05aa588c1099444af80400d4ae38129665d3
danny-pantoja <danny.pantoja.23@gmail.com>
2020-08-25 08:16:41 -0700

    Daniel added item.js

---
+++ app/models/item.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  iamgeUrl: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  stock: {
+    type: Number,
+  },
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'School',
+    }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Item', itemSchema)
