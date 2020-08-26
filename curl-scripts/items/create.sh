4937bbc43500d2132fbb2c334e38c7641091c280
rubysattar <rubysattar@gmail.com>
2020-08-26 14:49:10 -0400

    Ruby update create curl-script

--- curl-scripts/items/create.sh
+++ curl-scripts/items/create.sh
@@ -9,7 +9,8 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
-    "name": {
+    "item": {
+      "name":"'"${NAME}"'",
       "price": "'"${PRICE}"'"
     }
   }'
