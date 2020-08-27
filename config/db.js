9757f1cc471a09086229f39ba39a0826c261db85
Sebastian <sebastian.chang23@gmail.com>
2020-08-27 09:42:29 -0500

    Updated db.js to rename database name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'educational-store'
 
 // create the mongodb uri for development and test
 const database = {
